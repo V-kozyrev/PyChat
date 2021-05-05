30e9f9a9d4dcfe8769469ef4c9e603fe09eb761c
V-kozurev <v4.kozyrev@gmail.com>
2021-05-05 12:16:13 +0300

    add run_client

--- Client/src/main.py
+++ Client/src/main.py
@@ -3,5 +3,16 @@ from auntification import entry_process
 from constants import ConnectionConstants, logger
 
 
+def run_client():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # socket initialization
+
+    try:
+        client.connect((ConnectionConstants.host, ConnectionConstants.port))  # connecting client to server
+        entry_process(client)
+    except ConnectionRefusedError as e:
+        logger.error("the server is down!")
+        client.close()
+
+
 if __name__ == '__main__':
     run_client()
